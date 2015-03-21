9173f194c2ef89259f0d3409a4b02b922a15b28d
James <edwardsj9090@gmail.com>
2015-03-20 20:09:31 -0400

    Update SysUpdates.sh

--- SysUpdates.sh
+++ SysUpdates.sh
@@ -1,12 +1,1 @@
-#!/bin/bash
 
-# This is an update script for any flavor of Linux running the apptitude package manager.
-
-# Downloads updates from the repository sources for your Linux distribution and installs them
-sudo apt-get update && sudo apt-get upgrade -y
-
-# Downloads and installs any updates to the OS
-sudo apt-get dist-upgrade -y
-
-# Updates Linux headers
-sudo apt-get update && sudo apt-get install -y linux-headers-$(uname -r)

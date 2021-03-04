4f62b5ca7464f9cb425bff3fbdd91ac8534b7675
runlevel4 <edwardsj9090@gmail.com>
2021-03-04 17:11:48 -0500

    Add files via upload

---
+++ installlampwithfirefly.sh
@@ -0,0 +1,97 @@
+#!/bin/sh
+
+echo "This will install the LAMP (Linux, Apache, MySQL/MariaDB, PHP/Perl) stack along with the firefly-iii financial manager app..."
+echo ""
+echo "This script assumes 3 things:"
+echo "1. You are running apache so the apache2 service directory is /etc/apache2/"
+echo "2. Your web root directory is /var/www/html/"
+echo "3. You have a root password for your mysql instance"
+sleep 10
+echo ""
+echo ""
+echo "First start by making sure that all of the packages are up to date..."
+echo ""
+echo ""
+sudo apt update && sudo apt upgrade -y
+echo ""
+echo ""
+echo "Very nice...now we install the necessary web server components..."
+sleep 3
+echo ""
+echo ""
+sudo apt install apache2 mysql-common mariadb-server php7.4 php7.4-bcmath php7.4-intl php7.4-curl php7.4-zip php7.4-gd php7.4-xml php7.4-mbstring php7.4-ldap php7.4-mysql php-mysql -y
+echo ""
+echo ""
+echo "Now to install Composer (a friendly php helper that unpacks the php libraries contained within firefly and creates a firefly-ii directory with the project unpacked..."
+echo ""
+echo ""
+sudo curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+echo ""
+echo ""
+cd /var/www/html
+echo ""
+echo ""
+echo "If prompted, just hit Enter"
+echo ""
+echo ""
+sudo composer create-project grumpydictator/firefly-iii --no-dev --prefer-dist firefly-iii 5.4.6
+echo ""
+echo ""
+echo "Change firefly-iii directory permissions"
+echo ""
+echo ""
+# This will stop the  white screen issue
+sudo chown -R www-data:www-data firefly-iii
+sudo chmod -R 775 firefly-iii/storage
+echo ""
+echo ""
+echo "Creating firefly database and user..."
+echo ""
+echo ""
+sudo mysql -u root -p < /home/scripty/mysql_setup.txt
+echo ""
+echo ""
+echo "Replacing .env file in /var/www/html directory (changes the db_host parameter to localhost)..."
+echo ""
+echo ""
+sudo cp /home/scripty/.env /var/www/html/firefly-iii/
+echo ""
+echo ""
+cd /var/www/html/firefly-iii
+echo ""
+echo ""
+echo "Artisan setup..."
+echo ""
+echo ""
+sudo php artisan migrate:refresh --seed
+sudo php artisan firefly-iii:upgrade-database
+sudo php artisan passport:install
+echo ""
+echo ""
+echo "All done..."
+echo ""
+echo ""
+echo "Post Install Steps:"
+echo ""
+echo ""
+echo "Make sure that the following lines are in the /etc/apache2/apache2.conf file"
+echo "This will protect the root of the firefly-iii folder from prying eyes :)"
+echo ""
+echo ""
+echo "<Directory /var/www/>"
+echo     "Options Indexes FollowSymLinks"
+echo     "AllowOverride All"
+echo     "Require all granted"
+echo "</Directory>"
+echo ""
+echo ""
+echo "<Directory /var/www/html/firefly-iii>"
+echo     "Require all denied"
+echo "</Directory>"
+echo ""
+echo ""
+echo "<Directory /var/www/html/firefly-iii/public>"
+echo     "Require all granted"
+echo "</Directory>"
+echo ""
+echo ""

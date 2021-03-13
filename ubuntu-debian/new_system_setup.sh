a9a7328cb2127c97e6b93b74535dc72b37448bd3
runlevel4 <edwardsj9090@gmail.com>
2021-03-12 21:19:41 -0500

    Update and rename new_system_setup.sh to ubuntu-debian/new_system_setup.sh

--- ubuntu-debian/new_system_setup.sh
+++ ubuntu-debian/new_system_setup.sh
@@ -3,9 +3,15 @@ echo "This script assumes that you are running a flavor of Linux with the APT pa
 sleep 3
 echo
 echo "This will update your packages and your distribution's headers..."
+echo
 sudo apt update && sudo apt upgrade && sudo apt dist-upgrade && sudo apt upgrade-linux-headers-$(uname -r) -y
 echo
 echo "Installing uncomplicated firewall..."
+echo
 sudo apt install ufw
+echo
+echo "Installing BleachBit"
+echo
+sudo apt install bleachbit
 echo
 echo "System updates and firewall protection have been installed.  Have fun :)"
